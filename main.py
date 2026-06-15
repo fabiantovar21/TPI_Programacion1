5ba23ae79aa3cab33cd01b21984d4f676e87cb7e
fabiantovar21 <fabian.tovar2002@gmail.com>
2026-06-15 00:13:56 -0300

    Actualizacion menu
    integre la opcion 6 de estadisticas

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from validaciones import validar_entero, validar_nombre
 from gestion import agregar_pais, actualizar_datos, buscar_por_nombre
 from archivos import cargar_paises_csv, guardar_paises_csv
+from estadisticas import extremos, prom_pob, prom_sup, cant_paises
 #Menú
 n=0
 nombre_archivo='paises.csv'
@@ -17,7 +18,7 @@ while n!=7:
         '7) Salir\n'))
         if n < 1 or n > 7:
             print('Por favor ingrese una opción válida 1-7')
-        if n==1:
+        elif n==1:
             nombre=validar_nombre('Ingrese el nombre del país: ')
             poblacion=validar_entero(f'Ingrese la poblacion de {nombre}: ')
             superficie=validar_entero(f'Ingrese la superficie de {nombre} en km2: ')
@@ -28,7 +29,7 @@ while n!=7:
                 guardar_paises_csv(nombre_archivo, lista_paises)
             else:
                 print(f"\n[Error]: No se pudo agregar. El país '{nombre}' ya se encuentra registrado en el sistema.")
-        if n==2:
+        elif n==2:
             nombre_buscado=validar_nombre('Ingrese el nombre del país que desea actualizar: ')
             nueva_pob=validar_entero('Ingrese la población: ')
             nueva_sup=validar_entero('Ingrese la superficie: ')
@@ -37,14 +38,34 @@ while n!=7:
                 guardar_paises_csv(nombre_archivo, lista_paises)
             else:
                 print(f'{nombre_buscado} no encontrado')
-        if n==3:
+        elif n==3:
             nombre_buscar=validar_nombre('Ingrese el país que desea buscar: ')
             coincidencia=buscar_por_nombre(lista_paises, nombre_buscar)
-        if n==4:
+        elif n==4:
             pass
-        if n==5:
+        elif n==5:
             pass
-        if n==6:
-            pass
+        elif n==6:
+            opcion=0
+            while opcion!=5:
+                try:
+                    opcion=int(input('Seleccione la estadistica que desea ver:\n' \
+                    '1) País con mayor y menor población\n' \
+                    '2) Promedio de población \n' \
+                    '3) Promedio de superficie\n' \
+                    '4) Cantidad de países por continente\n'
+                    '5) Volver\n'))
+                    if opcion < 1 or opcion > 5:
+                        print('Por favor ingrese una opción válida 1-5')
+                    elif opcion==1:
+                        extremos(lista_paises)
+                    elif opcion==2:
+                        prom_pob(lista_paises)
+                    elif opcion==3:
+                        prom_sup(lista_paises)
+                    elif opcion==4:
+                        cant_paises(lista_paises)
+                except ValueError as e:
+                    print(f'Error: {e}')
     except ValueError as e:
         print(f'Error: {e}')
